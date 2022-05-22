dc4bfe0a76e16fd9275aa81d6ecbe006c1b65e68
Egor Bondarevskii <egorbond31@yandex.ru>
2022-05-22 16:30:28 +0300

    remove comments

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,8 +1,1 @@
-// import { compose } from "redux";
 /// <reference types="react-scripts" />
-
-// declare global {
-//   interface Window {
-//     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-//   }
-// }
